1721e7ba435dc99e445d2324783c0c3b5c326110
wil pannell <wil.pannell@gmail.com>
2018-12-31 19:30:19 -0800

    expand jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,16 @@
 // https://github.com/thymikee/jest-preset-angular#brief-explanation-of-config
 module.exports = {
+  cacheDirectory: './.jest-cache',
+  coverageDirectory: 'coverage',
+  coverageReporters: ['json', 'lcov'],
+  globals: {
+    "ts-jest": {
+      'tsConfigFile': '<rootDir>/src/tsconfig.spec.json',
+      'skipBabel': true
+    },
+    "\_\_TRANSFORM_HTML\_\_": true
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
   moduleNameMapper: {
     '@app/(.*)': '<rootDir>/src/app/$1',
     '@assets/(.*)': '<rootDir>/src/assets/$1',
@@ -8,8 +19,14 @@ module.exports = {
     '@src/(.*)': '<rootDir>/src/src/$1',
     '@state/(.*)': '<rootDir>/src/app/state/$1',
   },
+  modulePaths: ['<rootDir>/src/'],
   preset: 'jest-preset-angular',
   roots: ['src'],
   setupTestFrameworkScriptFile: '<rootDir>/src/setup-jest.ts',
+  testMatch: ['**/+(*.)+(spec|test).+(ts|js)'],
+  testURL: 'http://localstorage:8080',
+  transform: {
+    '^.+\\.(ts|js|html)x?$': '<rootDir>/node_modules/jest-preset-angular/preprocessor.js'
+  },
   transformIgnorePatterns: ['node_modules/(?!(jest-test))'],
 };
