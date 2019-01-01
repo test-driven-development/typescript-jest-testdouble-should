50acac78c29663cd04eccc783d65b387c36545e3
wil pannell <wil.pannell@gmail.com>
2019-01-01 04:44:09 -0800

    purge deprecation warnings

--- jest.config.js
+++ jest.config.js
@@ -5,8 +5,8 @@ module.exports = {
   coverageReporters: ['json', 'lcov'],
   globals: {
     "ts-jest": {
-      'skipBabel': true,
-      'tsConfigFile': '<rootDir>/src/tsconfig.spec.json',
+      'babelConfig': false,
+      'tsConfig': '<rootDir>/src/tsconfig.spec.json',
     }
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
