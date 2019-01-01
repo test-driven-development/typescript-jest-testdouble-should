eb0d5e45db81d64e29d0404192a31eb085ee414b
wil pannell <wil.pannell@gmail.com>
2018-12-31 18:42:06 -0800

    alphabetize

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,5 @@
 // https://github.com/thymikee/jest-preset-angular#brief-explanation-of-config
 module.exports = {
-  preset: 'jest-preset-angular',
-  roots: ['src'],
-  setupTestFrameworkScriptFile: '<rootDir>/src/setup-jest.ts',
   moduleNameMapper: {
     '@app/(.*)': '<rootDir>/src/app/$1',
     '@assets/(.*)': '<rootDir>/src/assets/$1',
@@ -11,5 +8,8 @@ module.exports = {
     '@src/(.*)': '<rootDir>/src/src/$1',
     '@state/(.*)': '<rootDir>/src/app/state/$1',
   },
+  preset: 'jest-preset-angular',
+  roots: ['src'],
+  setupTestFrameworkScriptFile: '<rootDir>/src/setup-jest.ts',
   transformIgnorePatterns: ['node_modules/(?!(jest-test))'],
 };
